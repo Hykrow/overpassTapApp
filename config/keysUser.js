ff2065e039649b9f2aca7380e07121801685c77d
Hykrow <falsenoam@gmail.com>
2019-08-04 21:39:58 +0200

    removed security informations

--- config/keysUser.js
+++
@@ -1,5 +0,0 @@
-dbPassword = 'mongodb+srv://admin:Y2iynSuvZtk7IWx4@cluster0-p4eef.mongodb.net/test?retryWrites=true&w=majority';
-//Y2iynSuvZtk7IWx4
-module.exports = {
-    mongoURI: dbPassword
-};
